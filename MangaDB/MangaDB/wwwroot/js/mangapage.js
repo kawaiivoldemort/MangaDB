5ef10c527dc67a229ef1da21aef5e49e1a889675
xaphatro <xaphatro@gmail.com>
2017-04-07 00:19:52 +0530

    Trying to fetch data from the web service

--- MangaDB/MangaDB/wwwroot/js/mangapage.js
+++ MangaDB/MangaDB/wwwroot/js/mangapage.js
@@ -2,31 +2,42 @@ var $jq = jQuery.noConflict();
 
 $jq(document).ready(function() {
     //code to connect to DB
-    
     stageOneDownload = function() {
-        //code to download first json, stored in stageonedown
-
-        var metadata = jq.parseJSON(stageonedown);
-        $jq('#title').html();
-        $jq("#titleOriginal").html();
-        $jq("#author").html();
-        $jq("#publisher").html();
+        //code to download first json
+        $jq.ajax({dataType: "json", url: "metadata", success: function(data) {
+                metadata = jq.parseJSON(data);
+            }
+        });
+        $jq('#title').html(metadata.name);
+        $jq("#titleOriginal").html(metadata.japaneseName);
+        $jq("#author").html(metadata.author);
+        $jq("#publisher").html(metadata.publisher);
     }
+    /*
     stageTwoDownload = function() {
-        //code to download chapter list, stored in stagetwodown
-
-        stagetwodown.forEach(function(item) {
-            var chapter = jq.parseJSON(item);
+        //code to download chapter list
+        var chapters = jq.parseJSON(stagetwodown);
+        chapters.forEach(function(item) {
+            
             var chRow = document.createElement("div");
             var chNumber = document.createElement("div");
             var chName = document.createElement("div");
             $js(chRow).addClass('row');
             $js(chNumber).addClass('col-md-2');
             $js(chName).addClass('col-md-4');
+            $js(chRow).append(chNumber);
+            $js(chRow).append(chName);
             
         }, this);
-    }
+    }*/
     stageThreeDownload = function() {
         //code to download synopsis
+        $jq.ajax({dataType: "json", url: "summary", success: function(data) {
+                synopsis = jq.parseJSON(data);
+            }
+        });
+        $jq("#summary").html(synopsis.summary);
     }
+    stageOneDownload();
+    stageThreeDownload();
 })
